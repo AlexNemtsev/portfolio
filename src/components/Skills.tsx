20524064c51e358564e86497c7cd00858e36b3c9
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-09-18 21:01:56 +0300

    refactor: move skills into the component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,41 @@
-import { skills } from '../data/skills';
 import { FlexWrapper } from './FlexWrapper';
 import { Skill } from './Skill';
+
+interface skill {
+  iconId: string;
+  skillName: string;
+}
+
+const skills: skill[] = [
+  {
+    iconId: 'js',
+    skillName: 'javascript',
+  },
+  {
+    iconId: 'ts',
+    skillName: 'typescript',
+  },
+  {
+    iconId: 'jest',
+    skillName: 'jest',
+  },
+  {
+    iconId: 'react',
+    skillName: 'react',
+  },
+  {
+    iconId: 'styled',
+    skillName: 'styled components',
+  },
+  {
+    iconId: 'redux',
+    skillName: 'redux',
+  },
+  {
+    iconId: 'git',
+    skillName: 'git',
+  },
+];
 
 export const Skills = () => {
   return (
