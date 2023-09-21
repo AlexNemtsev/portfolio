379ba04c156ed87c8a442f9c192b8adf06ae0ad1
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-09-21 19:08:39 +0300

    refactor: move fonts from styles into html

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Tinos&display=swap');
-
 :root {
   font-family: 'Montserrat', sans-serif;
   color: #1F2626;
