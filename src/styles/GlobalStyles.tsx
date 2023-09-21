6d399c28f3b590cc6bbab9d2e61e4e328caf492c
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-09-21 19:06:07 +0300

    refactor: add GlobalStyles component

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Tinos&display=swap');
+
+:root {
+  font-family: 'Montserrat', sans-serif;
+  color: #1F2626;
+  font-weight: 400;
+}
+
+body {
+  margin: 0 auto;
+  background-color: #FAFAFA;
+}
+`;
