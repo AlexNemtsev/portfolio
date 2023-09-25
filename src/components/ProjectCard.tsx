f47cc75d85547e6538832f7bcb54a3a91ac075b3
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-09-25 15:55:39 +0300

    fix: fix project card styles

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,27 +9,29 @@ export const ProjectCard = (props: ProjectCardProps) => {
   return (
     <FlexWrapper as={Card} $direction="column" $align="flex-start" $maxWidth="530px">
       <img src={props.cover} alt="project card cover" width="100%" />
-      <CardTitle>{props.title}</CardTitle>
-      <FlexWrapper $columnGap="6px">
-        {props.skills.map((skill) => (
-          <Highlighted as={StyledSpan} key={skill}>
-            {skill}
-          </Highlighted>
-        ))}
+      <FlexWrapper as={ProjectInfo} $direction="column">
+        <CardTitle>{props.title}</CardTitle>
+        <FlexWrapper $columnGap="12px">
+          {props.skills.map((skill) => (
+            <Highlighted as={StyledSpan} key={skill}>
+              {skill}
+            </Highlighted>
+          ))}
+        </FlexWrapper>
+        <p>{props.description}</p>
       </FlexWrapper>
-      <p>{props.description}</p>
     </FlexWrapper>
   );
 };
 
 const StyledSpan = styled.span`
   text-transform: uppercase;
-  padding: 8px 12px;
+  padding: 8px 16px;
   border-radius: 4px;
 `;
 
 const CardTitle = styled.h3`
-  margin: 0;
+  margin: 0 0 10px;
   text-transform: uppercase;
 `;
 
@@ -44,4 +46,8 @@ const Card = styled.div`
     css`
       box-shadow: ${props.theme.projectCard.boxShadow};
     `};
+`;
+
+const ProjectInfo = styled.div`
+  padding: 30px 10px 30px 26px;
 `;
