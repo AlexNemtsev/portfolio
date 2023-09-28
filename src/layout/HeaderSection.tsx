39f3320ecae0b299ddf514bb47630680c59cd9ec
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-09-28 17:29:20 +0300

    refactor: remove Menu component

--- src/layout/HeaderSection.tsx
+++ src/layout/HeaderSection.tsx
@@ -1,9 +1,36 @@
-import { Menu } from '../components/Menu';
+import styled from 'styled-components';
+import { Container } from '../components/Container';
+import { StyledLink, HighlightedLink } from '../components/StyledLinks';
 
 export const HeaderSection = () => {
   return (
     <header>
-      <Menu />
+      <Container as={StyledMenu}>
+        <StyledLink href="#main">Home</StyledLink>
+        <ul>
+          <li>
+            <StyledLink href="#projects">Projects</StyledLink>
+          </li>
+          <li>
+            <HighlightedLink href="#contacts">Contact</HighlightedLink>
+          </li>
+        </ul>
+      </Container>
     </header>
   );
 };
+
+const StyledMenu = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ul {
+    margin: 40px 0;
+    display: flex;
+    justify-content: center;
+    gap: 90px;
+    padding: 0;
+    list-style: none;
+  }
+`;
